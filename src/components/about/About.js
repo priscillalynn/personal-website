0e0aec73d4697185b270e40dfae586af039ce49d
Priscilla Colon <plynn9036@gmail.com>
2022-08-04 15:54:58 -0400

    update about.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,14 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className=".container.about__container">
+        
+      </div>
+    </section>
   )
 }
 
