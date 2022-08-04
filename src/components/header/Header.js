0d613ad59d20581cd29a25883ae3275f7ca72883
Priscilla Colon <plynn9036@gmail.com>
2022-08-03 23:38:51 -0400

    edited header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import temp from './../../assets/temp.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Priscilla Colon</h1>
+            <h5 className="text-light">Computer Science Student / Aspiring Software Engineer </h5>
+            <CTA />
+            <HeaderSocials />
+
+            <div className="me">
+                <img src={temp} alt="me" />
+            </div>
+        </div>
+    </header>
   )
 }
 
