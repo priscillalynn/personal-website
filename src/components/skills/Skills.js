cee45c7fc30adf5f97b893c9a366dfbaaf06b0ba
Priscilla Colon <plynn9036@gmail.com>
2022-08-04 23:05:50 -0400

    updated about

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,9 +1,90 @@
 import React from 'react'
 import './skills.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills</section>
+    <section id='skills'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__languages">
+          <h3>Languages</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>HTML/CSS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Java</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>SQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        {/* END OF LANGUAGES */}
+
+        <div className="experience__tech">
+          <h3>Technologies</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Node JS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Android Studio</h4>
+              <small className='text-light'>Basic</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Eclipse</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>IntelliJ</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>NetBeans</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Visual Studio Code</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill />
+              <h4>Git</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
