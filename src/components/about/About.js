515b210842f2ba5050061cd025a9341503f0f6db
Priscilla Colon <plynn9036@gmail.com>
2022-08-06 08:54:41 -0400

    about.css attempt fix

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,22 +9,21 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className=".container.about__container">
-        <div className="about__me">
+        
           <div className="about__me-img">
             <img src={ME} alt="About Me" />
           </div>
-        </div>
-        <div className=".about__content">
+        
+          <div className="about__me">
           <p>
           I am currently a third year Computer Science major at Florida International University.
           For the upcoming 2022 Fall semester, I will be working on my own personal portfolio to gain the necessary skills to obtain my first Software Engineering internship.
           <br />
           Outside of work, I like to read fiction novels, do outdoor activities such as hiking, and play video games. 
-
           </p>
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
         </div>
-      </div>
     </section>
   )
 }
