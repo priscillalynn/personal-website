46620591a70135ae57ed10cb2c1901899d503069
Priscilla Colon <plynn9036@gmail.com>
2022-08-07 09:13:27 -0400

    updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import temp from './../../assets/pc.gif'
+import temp from './../../assets/pc3.gif'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
